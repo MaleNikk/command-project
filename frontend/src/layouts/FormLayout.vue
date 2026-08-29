aa0fecf75f110095b3f5a9d13ca7353366f02992
Карпов_Николай <karpov.nikolay.1986@gmail.com>
2026-08-29 15:55:07 +0300

    rename_front_layot_name_NETWORK_51_to_CONVERSATION SERVICE

--- frontend/src/layouts/FormLayout.vue
+++ frontend/src/layouts/FormLayout.vue
@@ -13,7 +13,6 @@
       <svg
         class="footer__waves"
         xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
         viewBox="0 24 150 28"
         preserveAspectRatio="none"
         shape-rendering="auto"
